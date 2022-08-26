bb93bbfc15623358601f7bf22b36ad5e4f79ab81
SteveGerr <prikolsamara@mail.ru>
2022-08-26 23:02:34 +0400

    [refactor] Add hooks imports

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import { ref } from 'vue'
+import { weatherData as wd} from "./interfaces/weatherI"
+import { useData } from './hooks/data'
+import { useCoords } from './hooks/coords'
+import { useDataURL } from './hooks/dataURL'
+import { useCheckData } from './hooks/checkData'
+
 createApp(App).mount('#app')
+export { ref, wd, useData, useCoords, useDataURL, useCheckData }
